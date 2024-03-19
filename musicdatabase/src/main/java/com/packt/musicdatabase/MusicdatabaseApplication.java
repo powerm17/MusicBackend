9c562722e1bfb31a8f85793905a0816a139b1987
powerm17 <105803016+powerm17@users.noreply.github.com>
2024-03-19 19:57:32 -0400

    Pom Update dependecy

--- musicdatabase/src/main/java/com/packt/musicdatabase/MusicdatabaseApplication.java
+++ musicdatabase/src/main/java/com/packt/musicdatabase/MusicdatabaseApplication.java
@@ -31,20 +31,20 @@ public class MusicdatabaseApplication {
 			arepository.save(album1);
 			arepository.save(album2);
 
-			Music music = new Music(
+			Music songs = new Music(
 					"https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=video&cd=&cad=rja&uact=8&ved=2ahUKEwiTwrTlnv-EAxXxC3kGHSbFBbsQtwJ6BAgNEAI&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3D2XqpXmhM1Fc&usg=AOvVaw3XuF6JXScLFHP_oWU_hWAm&opi=89978449",
 					"Don't Panic", "Coldplay", "https://m.media-amazon.com/images/I/61-m2ApVV4L._UF1000,1000_QL80_.jpg",
 					2000, album1);
-			repository.save(music);
-			music = new Music(
+			repository.save(songs);
+			songs = new Music(
 					"https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwi0nanmn_-EAxUGDHkGHRBuBNsQ78AJegQIExAB&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DfZcAQ0kuw1s&usg=AOvVaw2qIK4BSct2Dy02c39OSO1f&opi=89978449",
 					"Rosemary", "Deftones", "https://i.ytimg.com/vi/6Ii8E21rbvQ/hqdefault.jpg", 2012, album2);
-			repository.save(music);
-			music = new Music(
+			repository.save(songs);
+			songs = new Music(
 					"https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=video&cd=&cad=rja&uact=8&ved=2ahUKEwiTwrTlnv-EAxXxC3kGHSbFBbsQtwJ6BAgNEAI&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3D2XqpXmhM1Fc&usg=AOvVaw3XuF6JXScLFHP_oWU_hWAm&opi=89978449",
 					"Sparks", "Coldplay", "https://m.media-amazon.com/images/I/61-m2ApVV4L._UF1000,1000_QL80_.jpg",
 					2000, album1);
-			repository.save(music);
+			repository.save(songs);
 		};
 	}
 }
--- musicdatabase/src/main/java/com/packt/musicdatabase/domain/MusicRepository.java
+++ musicdatabase/src/main/java/com/packt/musicdatabase/domain/MusicRepository.java
@@ -1,6 +1,13 @@
 package com.packt.musicdatabase.domain;
 
 import org.springframework.data.repository.CrudRepository;
+import java.util.List;
 
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
 public interface MusicRepository extends CrudRepository<Music, Long> {
+	List<Music> findByTitle(@Param("title") String title);
+	List<Music> findByArtist(@Param("artist") String artist);
 }
