7354f80796e149a1aa93e68258f92d771fb76ae0
powerm17 <105803016+powerm17@users.noreply.github.com>
2024-03-18 21:11:23 -0400

    Music Data Base Application
    Import maven project dependencies. Create MusicdatabaseApplication.

---
+++ musicdatabase/src/main/java/com/packt/musicdatabase/MusicdatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.packt.musicdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicdatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicdatabaseApplication.class, args);
+	}
+
+}
---
+++ musicdatabase/src/test/java/com/packt/musicdatabase/MusicdatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.packt.musicdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicdatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
