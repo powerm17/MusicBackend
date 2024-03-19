847032b3a6f3efe34cff1d538841e8fe185ffd7e
powerm17 <105803016+powerm17@users.noreply.github.com>
2024-03-19 01:12:27 -0400

    Update

--- musicdatabase/src/main/java/com/packt/musicdatabase/domain/AlbumRepository.java
+++ musicdatabase/src/main/java/com/packt/musicdatabase/domain/AlbumRepository.java
@@ -2,6 +2,6 @@ package com.packt.musicdatabase.domain;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface AlbumRepository extends CrudRepository<Album, Long>       
+public interface AlbumRepository extends CrudRepository<Album, Long>
 {
 }
